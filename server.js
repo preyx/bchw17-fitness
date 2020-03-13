2678828fcba5681dcc7328ed2f67a7d52c542071
Bryan Y <xpreyx@gmail.com>
2020-03-13 01:10:38 -0700

    tweak serverjs yet again

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const { join } = require('path')
-// const { Workout } = require('./models')
 const app = express()
 
 app.use(express.static(join(__dirname, 'public')))
@@ -10,15 +9,15 @@ app.use(express.json())
 app.use(require('./routes'))
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, './public/index.html'))
+  res.sendFile(join(__dirname, '/public/index.html'))
 })
 
 app.get('/exercise', (req, res) => {
-  res.sendFile(join(__dirname, './public/exercise.html'))
+  res.sendFile(join(__dirname, '/public/exercise.html'))
 })
 
 app.get('/stats', (req, res) => {
-  res.sendFile(join(__dirname, './public/stats.html'))
+  res.sendFile(join(__dirname, '/public/stats.html'))
 })
 
 require('./config')
